31a2d314ac05d6b73a95f2457bb2bb7edf08ebe8
Anushikha <anushikhakundu@gmail.com>
2025-11-06 20:39:35 +0530

    Implement waitBeforeExiting logic and activity.finish()

--- app/src/main/java/com/example/birdshooter/GameView.java
+++ app/src/main/java/com/example/birdshooter/GameView.java
@@ -247,8 +247,7 @@ public class GameView extends SurfaceView implements Runnable {
     private void waitBeforeExiting() {
         try {
             Thread.sleep(3000);
-            activity.finish();// new
-
+            activity.finish();// new finish
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
