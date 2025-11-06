5fca7625a48f3e2ef505c4e45d6bb6b801308357
Anushikha <anushikhakundu@gmail.com>
2025-11-06 20:23:48 +0530

    New Finish

--- app/src/main/java/com/example/birdshooter/GameView.java
+++ app/src/main/java/com/example/birdshooter/GameView.java
@@ -247,7 +247,6 @@ public class GameView extends SurfaceView implements Runnable {
     private void waitBeforeExiting() {
         try {
             Thread.sleep(3000);
-            activity.startActivity(new Intent(activity, MainActivity.class));
             activity.finish();
         } catch (InterruptedException e) {
             e.printStackTrace();
